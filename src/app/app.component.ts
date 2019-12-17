c82540fabe5cbc8e17bde6e6155eaeea2cb1f039
Kirezi <pnzey005@uottawa.ca>
2019-12-17 17:11:07 -0500

    changes added

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
+})
+export class AppComponent {
+  title = "tick-tac-toe";
+  menuItems = "Start New Game";
+}
